745b3c133614a9704e82c810c75d67574de1eb78
Clément Poncet <contact.clementponcet@gmail.com>
2020-09-08 15:42:47 +0200

    homepage resp check 50% + .env

--- components/layout.js
+++ components/layout.js
@@ -2,9 +2,18 @@ import React from "react";
 import Header from "./header";
 import Footer from "./footer";
 
+import Head from "next/head";
+
 function Layout(props) {
   return (
     <div className={`${props.page} relative`}>
+      <Head>
+        <title>Collectif 201</title>
+
+        {process.env.NODE_ENV === "development" ? (
+          <meta name="robots" content="noindex" />
+        ) : null}
+      </Head>
       <Header />
 
       <main>{props.children}</main>
